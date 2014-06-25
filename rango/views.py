c23bc874d4bf88b70dc656203594076002a6e457
Sourabh Dev <sourabh.dev90@gmail.com>
2014-06-25 23:46:45 +0530

    Logout implemented

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
@@ -143,3 +143,8 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/rango/')
