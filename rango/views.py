93b0fd3a4466e020b233c5f239dfd2d8c8da072e
Sourabh Dev <sourabh.dev90@gmail.com>
2014-06-30 14:29:35 +0530

    Added Bing Search

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.utils import encode, decode
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from rango.bing_search import run_query
 
 
 def index(request):
@@ -160,3 +161,15 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/rango/')
+
+def search(request):
+    context = RequestContext(request)
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            result_list = run_query(query)
+
+    return render_to_response('rango/search.html', {'result_list': result_list}, context)
